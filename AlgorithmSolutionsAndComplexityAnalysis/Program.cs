598191a636aab3ce11dd428bec232c64f8383b4d
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-22 17:54:42 +0400

    the best way to solve in Find the Missing Number in an Array

--- AlgorithmSolutionsAndComplexityAnalysis/Program.cs
+++ AlgorithmSolutionsAndComplexityAnalysis/Program.cs
@@ -25,17 +25,17 @@ namespace AlgorithmSolutionsAndComplexityAnalysis
             { if (numbers[i] > maxNumber) maxNumber = numbers[i]; }//O(n) in numbers[]
             //O(maxNumber)
             int[] totalNumber= new int[maxNumber];
-            for (int i = 0; i <= totalNumber.Length; i++)
+            for (int i = 0; i < totalNumber.Length; i++)
             {
                 totalNumber[i] = i;//totalNumber array has size O(maxNumber)
             }
             List<int> missNumber = new List< int > ();
             bool isThere;
-            for (int i = 0; i <= totalNumber.Length; i++) //O(maxNumber)
+            for (int i = 0; i < totalNumber.Length; i++) //O(maxNumber)
             {
                 isThere = false;
                 for (int j = 0;j<numbers.Length; j++)//O(maxNumber * n)
-                { if (totalNumber[i] == numbers[j])
+                { if (totalNumber[i] != numbers[j])
                     { isThere = true; } }
                 if (isThere != false) { missNumber.Add(totalNumber[i]); }
             }
@@ -44,6 +44,36 @@ namespace AlgorithmSolutionsAndComplexityAnalysis
             {
                 Console.WriteLine(missNumber[i]);
             }
+
+            ///the best way
+            ///
+
+            Console.WriteLine("enter the n of numbers to random: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int[] numbers1 = new int[n1];
+            for (int i = 0; i < n; i++) { numbers1[i] = int.Parse(Console.ReadLine()); }
+            int maxNumber1 = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers1[i] > maxNumber1)
+                {
+                    maxNumber1 = numbers1[i];
+                }
+            }
+            bool[] present = new bool[maxNumber + 1];
+            for (int i = 0; i < n; i++)
+            {
+                present[numbers1[i]] = true;
+            }
+            Console.WriteLine("Missing numbers between 0 and " + maxNumber1 + ":");
+            for (int i = 0; i <= maxNumber1; i++)
+            {
+                if (!present[i])
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
         }
     }
 }
